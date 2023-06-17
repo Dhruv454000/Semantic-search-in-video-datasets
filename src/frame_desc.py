215bead3b3eb50ca2964e0932298c1f7b1df415b
Dhruv454000 <dhruvkunjadiya55@gmail.com>
2023-06-17 22:37:19 +0530

    added code for finding frame desc

--- src/frame_desc.py
+++ src/frame_desc.py
@@ -1,74 +1,89 @@
+import argparse
 import json
-import pathlib
 import subprocess
-import gradio as gr
 import os
 from PIL import Image
 import open_clip
 import torch
-from sentence_transformers import SentenceTransformer
+import time
 
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-
+# Initialize the model
 model, _, transform = open_clip.create_model_and_transforms(
     "coca_ViT-L-14",
     pretrained="mscoco_finetuned_laion2B-s13B-b90k"
 )
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 def output_generate(image):
     im = transform(image).unsqueeze(0).to(device)
+    model.to(device)
+    im = im.float()
     with torch.no_grad(), torch.cuda.amp.autocast():
         generated = model.generate(im, seq_len=20)
-    return open_clip.decode(generated[0].detach()).split("<end_of_text>")[0].replace
-    ("<start_of_text>", "")
+    return open_clip.decode(generated[0].detach()).split("<end_of_text>")[0].replace("<start_of_text>", "")
 
-    return(output_generate(image))
 
-folder_path = "frames"
-
-def get_image(frames):
+def get_image(frames, video_file, folder_path):
     ffmpeg_commands = []
-    for i,frame in enumerate(frames):
-        ffmpeg_commands.append(f"ffmpeg -ss {frame} -i ../videos_dhruv/2016-02-02_1500_US_KABC_Good_Morning_America.mp4 -vframes 1 ../frames/{i}.jpg")
+    for i, frame in enumerate(frames):
+        ffmpeg_commands.append(f"ffmpeg -ss {frame} -i {video_file} -vframes 1 {folder_path}/{frame}.jpg")
     for command in ffmpeg_commands:
         subprocess.run(command, shell=True)
 
-# Load data from JSON file
-with open('../output_data/2016-02-02_1500_US_KABC_Good_Morning_America.v4.json') as f:
-    data = json.load(f)
 
-# Iterate over sentences
-for sent in data['sentences']:
-    frames = []
-    starttime = sent['starttime']
-    endtime = sent['endtime']
-    frames.append(starttime)
-    frames.append(endtime)
-    
-    # Calculate midtime
-    midtime = (float(starttime) + float(endtime)) / 2
-    
-    frames.append(midtime)
-    if(sent['verbs'] != []):
-        for verb in sent['verbs']:
-            frames.append(verb['vstart'])
-            
-    # Call get_image function with the calculated values
-    get_image(frames)
-    
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
+def process_video_frames(video_file, json_file, folder_path):
+    with open(json_file) as f:
+        data = json.load(f)
 
-        # Check if the current item is a file
-        if os.path.isfile(file_path):
-            # Open the image using PIL
-            image = Image.open(file_path)
-            print(output_generate(image))
-    break
+    for sent in data['sentences']:
+        frames = []
+        starttime = sent['starttime']
+        endtime = sent['endtime']
+        frames.append(starttime)
+        frames.append(endtime)
+        midtime = (float(starttime) + float(endtime)) / 2
+        frames.append(midtime)
+        if sent['verbs'] != []:
+            for verb in sent['verbs']:
+                if verb not in frames:
+                    frames.append(verb['vstart'])
         
-
+        get_image(frames, video_file, folder_path)
         
-     
+        frame_data = []
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+
+            if os.path.isfile(file_path):
+                image = Image.open(file_path)
+                frame_text = output_generate(image)
+                print(frame_text, 'f')
+                frame_data.append(frame_text)
+                os.remove(file_path)
+        
+        frame_data = unique(frame_data)
+        sent['frame_data'] = frame_data
     
-    
-    
+    with open(json_file, 'w') as f:
+        json.dump(data, f, indent=1)
+
+
+def main(video_file, json_file, folder_path):
+
+    # Process video frames
+    process_video_frames(video_file, json_file, folder_path)
+
+    # End measuring time
+    end_time = time.time()
+    execution_time = end_time - start_time
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process video frames and generate frame data")
+    parser.add_argument("video_file", type=str, help="Input video file path")
+    parser.add_argument("json_file", type=str, help="Input JSON file path")
+    parser.add_argument("folder_path", type=str, help="Folder path to store frames")
+    args = parser.parse_args()
+
+    main(args.video_file, args.json_file, args.folder_path)
